c8a41bdb8ff484f6af9f92066cff3c31da21a201
Sandoval Melo <juniormelo777@gmail.com>
2022-07-04 17:36:52 -0300

    add validation feedback styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 const form = document.getElementById("form");
 const emailInput = document.getElementById("email");
 const errorIcon = document.querySelector(".error-icon");
-const errorMessage = document.querySelector(".error-message");
+const feedBack = document.querySelector(".feedback-message");
+const submitButton = document.querySelector(".submit-btn");
 
 function validateEmail(email) {
 	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
@@ -13,17 +14,35 @@ function validateEmail(email) {
 
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
+
 	const isEmailValid = validateEmail(emailInput.value);
 
 	if (!isEmailValid) {
 		errorIcon.style.display = "block";
-		errorMessage.style.display = "block";
+		feedBack.innerText = "Please provide a valid email";
+		feedBack.style.display = "block";
+		submitButton.disabled = true;
 
 		setTimeout(() => {
 			errorIcon.style.display = "none";
-			errorMessage.style.display = "none";
-		}, 5000);
+			feedBack.style.display = "none";
+			feedBack.innerContent = "";
+			submitButton.disabled = false;
+		}, 2000);
+
+		emailInput.focus();
+
+		return;
 	}
 
-	console.log("valid email");
+	feedBack.innerText = "Your subscription has been confirmed";
+	feedBack.style.display = "block";
+	feedBack.classList.add("success");
+	emailInput.value = "";
+
+	setTimeout(() => {
+		feedBack.style.display = "none";
+		feedBack.innerContent = "";
+		feedBack.classList.remove("success");
+	}, 2000);
 });
