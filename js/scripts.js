3ac40fcbb810d724a77e3b59bf9c04866ac70644
Sandoval Melo <juniormelo777@gmail.com>
2022-07-04 16:09:58 -0300

    add validation form

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
 const form = document.getElementById("form");
+const emailInput = document.getElementById("email");
+const errorIcon = document.querySelector(".error-icon");
+const errorMessage = document.querySelector(".error-message");
+
+function validateEmail(email) {
+	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+		return true;
+	}
+
+	return false;
+}
 
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
-	console.log(event);
+	const isEmailValid = validateEmail(emailInput.value);
+
+	if (!isEmailValid) {
+		errorIcon.style.display = "block";
+		errorMessage.style.display = "block";
+
+		setTimeout(() => {
+			errorIcon.style.display = "none";
+			errorMessage.style.display = "none";
+		}, 5000);
+	}
+
+	console.log("valid email");
 });
